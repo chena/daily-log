f75b8264471b2c1084e5822dd902fdfc2ae14290
Alice Chen <chiachun78@gmail.com>
2014-07-22 18:08:50 -0400

    Added documentations and reload data when dates change

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+// TODO: make app an object, encapsulate functions
+
 function removeItems() {
 	$('#list').find('li').remove();
 }
@@ -37,27 +39,24 @@ function loadLocalData() {
 		var matched = $('.day').filter(function() { 
 		  return $(this).data('log') == key;
 		});
+
 		if (matched.length > 0) {
-			matched.find('li').remove();
+			matched.find('li').remove(); // first remove everything
 			var arr = JSON.parse(localStorage.getItem(key));
 			arr.forEach(function(text) {
 				matched.append($('<li>', {
 					text: text
 				}));
-				
 			});	
 		}
 	}
 }
 
-
-
 /********************
   Script starts
 ********************/
 
-// set default dates
-//var startDate = moment().subtract('d', 7),
+// set default dates, set default start date to first day of hacker school
 var startDate = moment('20140721', 'YYYYMMDD'),
 	endDate = moment().add('d', 7);
 
@@ -66,33 +65,37 @@ updateDates(startDate, endDate);
 // set change events for date range
 $('#startDate').on('change', function(e) {
 	startDate = moment($(e.target).val());
+	// change end date is start dates becomes greater than endate
 	if (startDate.isAfter(endDate)) {
 		endDate = moment(startDate).add('days', 7);
 		updateDates(null, endDate);
 	}
 	generateDailyBlocks();
+	loadLocalData();
 });
 
 $('#endDate').on('change', function(e) {
 	endDate = moment($(e.target).val());
+	// change start date if end date becomes earlier than start date
 	if (endDate.isBefore(startDate)) {
 		startDate = moment(endDate).subtract('days', 7);
 		updateDates(startDate, null);
 	}
 	generateDailyBlocks();
+	loadLocalData();
 });
 
 $('#list').on('keydown', function(e) {
 	var target = $(e.target),
 		items = target.find('li');
 
-	// append a new item if none exists
+	// prevent deleting if there is only one empty il element left
 	if (items.length == 1 && e.keyCode === 8 && items.text() === '') {	
 		e.preventDefault();
 		return;
 	}
 
-	// TODO: should make a button to save instead
+	// FIXME: rewrite every single time?
 	if (e.which === 13) {
 		var log = [];
 		target.find('li').each(function(index, item) {
@@ -100,14 +103,10 @@ $('#list').on('keydown', function(e) {
 		});
 		localStorage.setItem(target.data('log'), JSON.stringify(log));
 	}
-
 });
-
 
 generateDailyBlocks();
 loadLocalData();
 
 // FIXME: chaning dates = buggy
-// TODO: advance by week
-// TODO: localStorage
-// TODO: paginate
+// TODO: advance by week or paginate?
