48cde4cb85d49d368ffb5af996896f71c27d3e45
Alice Chen <chiachun78@gmail.com>
2014-07-25 16:45:51 -0400

    Refactoring by creating the app as an object

--- static/script.js
+++ static/script.js
@@ -1,97 +1,105 @@
-// TODO: make app an object, encapsulate functions
+'use strict';
+
+/*
+var App = (function(moment){
+function DailyLogApp() {
+// constructor
+}
+DailyLogApp.prototype = {
+someFunction: function() {}
+}
+return DailyLogApp;
+})(moment);
+var app = new App();
+function updateDates() {
+$('#')
+};
+$('#endDate').on('change', function(e) {
+app.changeStartDate($(e.target).val());
+updateDates();
+});
+*/
+
 var App = function() {
-	// set default dates, set default start date to first day of hacker school
-	this.getDefaultDates = function() {
-		return {
-			startDate:  moment('20140721', 'YYYYMMDD'),
-			endDate: moment().add('d', 7)
-		};
-	};
-
-	this.removeItems = function() {
-		$('#list').find('li').remove();
-	};
-
-	this.generateDailyBlocks = function() {
-		this.removeItems();
-		var defaultDates = this.getDefaultDates();
-
-		for (var currDate = defaultDates.startDate; 
-				!currDate.isAfter(defaultDates.endDate); 
-				currDate = currDate.add('d', 1)) {
-			var item = $('<li>')
-				.append($('<label>', {
-					text: currDate.format('MM/DD (ddd)')
-				}));
-
-			var log = $('<ul>', {
-					'contenteditable': true,
-					'class': 'day',
-					'data-log': currDate
-				}).append($('<li>'));
-
-			item.append(log);
-			$('#list').append(item);
-		}
-	};
-
-	this.updateDates = function(startDate, endDate) {
-		if (startDate) {
-			$('#startDate').val(startDate.format('YYYY-MM-DD'));
-		}
-
-		if (endDate) {
-			$('#endDate').val(endDate.format('YYYY-MM-DD'));
-		}
-	};
-
-	this.loadLocalData = function() {
-		for (var key in localStorage) {
-			var d = new Date(Number.parseInt(key));
-			var matched = $('.day').filter(function() { 
-			  return $(this).data('log') == key;
-			});
-
-			if (matched.length > 0) {
-				matched.find('li').remove(); // first remove everything
-				var arr = JSON.parse(localStorage.getItem(key));
-				arr.forEach(function(text) {
-					matched.append($('<li>', {
-						text: text
-					}));
-				});	
-			}
-		}
-	};
-
-	this.startDateChanged = function(e) {
-
-	};
-
+	this.setDefaultDates();
+	// App.prototype.setDefaultDates.call(this);
+	this.updateDates(this.startDate, this.endDate);
 };
 
+// set default dates, set default start date to first day of hacker school
+App.prototype.setDefaultDates = function() {
+	this.startDate = moment('20140721', 'YYYYMMDD');
+	this.endDate = moment().add('d', 7)
+};
 
-/********************
-  Script starts
-********************/
-var app = new App();
-var defaultDates = app.getDefaultDates();
-app.updateDates(defaultDates.startDate, defaultDates.endDate);
+App.prototype.removeItems = function() {
+	$('#list').find('li').remove();
+};
 
-// set change events for date range
-$('#startDate').on('change', function(e) {
-	startDate = moment($(e.target).val());
-	// change end date is start dates becomes greater than endate
-	if (startDate.isAfter(endDate)) {
-		endDate = moment(startDate).add('days', 7);
-		updateDates(null, endDate);
+App.prototype.generateDailyBlocks = function() {
+	this.removeItems();
+	for (var currDate = this.startDate; 
+			!currDate.isAfter(this.endDate); 
+			currDate = currDate.add('d', 1)) {
+		var item = $('<li>')
+			.append($('<label>', {
+				text: currDate.format('MM/DD (ddd)')
+			}));
+
+		var log = $('<ul>', {
+				'contenteditable': true,
+				'class': 'day',
+				'data-log': currDate
+			}).append($('<li>'));
+
+		item.append(log);
+		$('#list').append(item);
 	}
-	generateDailyBlocks();
-	loadLocalData();
-});
+};
 
-$('#endDate').on('change', function(e) {
-	endDate = moment($(e.target).val());
+App.prototype.updateDates = function(startDate, endDate) {
+	if (startDate) {
+		$('#startDate').val(startDate.format('YYYY-MM-DD'));
+	}
+
+	if (endDate) {
+		$('#endDate').val(endDate.format('YYYY-MM-DD'));
+	}
+};
+
+App.prototype.loadLocalData = function() {
+	for (var key in localStorage) {
+		var d = new Date(Number.parseInt(key));
+		var matched = $('.day').filter(function() { 
+		  return $(this).data('log') == key;
+		});
+
+		if (matched.length > 0) {
+			matched.find('li').remove(); // first remove everything
+			var arr = JSON.parse(localStorage.getItem(key));
+			arr.forEach(function(text) {
+				matched.append($('<li>', {
+					text: text
+				}));
+			});	
+		}
+	}
+};
+
+App.prototype.startDateChanged = function(value) {
+	// set change events for date range
+	this.startDate = moment(value);
+	// change end date is start dates becomes greater than endate
+	if (this.startDate.isAfter(this.endDate)) {
+		this.endDate = moment(this.startDate).add('days', 7);
+		this.updateDates(null, this.endDate);
+	}
+	this.generateDailyBlocks();
+	thisloadLocalData();
+};
+
+App.prototype.endDateChanged = function(value) {
+	endDate = moment(value);
 	// change start date if end date becomes earlier than start date
 	if (endDate.isBefore(startDate)) {
 		startDate = moment(endDate).subtract('days', 7);
@@ -99,7 +107,22 @@ $('#endDate').on('change', function(e) {
 	}
 	generateDailyBlocks();
 	loadLocalData();
+}
+
+
+/********************
+  Script starts
+********************/
+var app = new App();
+
+$('#startDate').on('change', function(e) { 
+	app.startDateChanged($(e.target).val()); 
 });
+
+$('#endDate').on('change', function(e) {
+	app.endDateChanged($(e.target).val());
+});
+
 
 $('#list').on('keydown', function(e) {
 	var target = $(e.target),
