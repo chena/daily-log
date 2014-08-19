99e6d1d44adf3e322b568b464ccee68ef3ed5090
Alice Chen <chiachun78@gmail.com>
2014-08-18 22:49:25 -0400

    Modified app main for heroku deploy

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return render_template('index.html',)
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
