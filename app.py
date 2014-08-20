b04f239ce32116e8b9cda2831390080cdf4a3463
Alice Chen <chiachun78@gmail.com>
2014-08-20 11:10:39 -0400

    Use port 3000 instead of 5000

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello():
     return render_template('index.html')
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 3000))
 	app.run(host='0.0.0.0', port=port)
